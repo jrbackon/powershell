4527a46575c24413c87e5526e403f5fd106ee7cb
Jacob Backon <jbackon@babson.edu>
2022-11-02 17:15:52 -0400

    script works as intended

--- users_type.ps1
+++ users_type.ps1
@@ -1,17 +1,27 @@
-$importedList = Get-Content -Path './test.txt'
+$importedList = Get-Content -Path './trash-users-2022-10-20.csv'
 $userResults = @()
 
 foreach($object in $importedList)
 {
     $username = $object.Split(",")[0]
+    $type = ''
 
     try {
-        Get-ADUser $username | Select-Object DistinguishedName
+        $dname = Get-ADUser $username | Select-Object DistinguishedName | Out-String
+        foreach($item in $dname.Split(','))
+        {
+            if($item -like "OU*"){
+                $type += $item + ', '
+            }
+        }
     }
     catch {
         $type = 'NA'
     }
     $userResults += [PSCustomObject]@{
+        Username = $username
         Type = $type
     }
 }
+
+$userResults | Export-Csv UserTypes.csv -NoTypeInformation
