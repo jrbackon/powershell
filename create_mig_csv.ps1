877ebcb391a61d26135f3b106a044828dee1d2b0
Jacob Backon <jbackon@babson.edu>
2023-02-02 15:33:34 -0500

    Simplified code based on username input.

--- create_mig_csv.ps1
+++ create_mig_csv.ps1
@@ -1,20 +1,10 @@
 $importedList = Get-Content -Path './test.txt'
 $migrations = @()
 
-foreach($name in $importedList){
-    $fname = $name.Split(" ")[0]
-    $lname = $name.Split(" ")[1]
-
-    $name = Get-ADUser -Filter {GivenName -eq $fname -and Surname -eq $lname} | Select Name
-    $sname = Out-String -InputObject $name
-    $username = $sname.Split([Environment]::NewLine)[6]
-
-    $mail = Get-ADUser -Filter {GivenName -eq $fname -and Surname -eq $lname} | Select UserPrincipalName
-    $smail = Out-String -InputObject $mail
-    $email = $smail.Split([Environment]::NewLine)[6]
+foreach($username in $importedList){
 
     $FSPath = "\\Fileshare2\Staff-FacultyFiles\$username"
-    $ODEMail = $email
+    $ODEMail = "$username@babson.edu"
 
     $migrations += [PSCustomObject]@{
         FileSharePath = $FSPath
